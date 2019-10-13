566791291de1836808c11d3405a381ac0600570c
Rainowy <tomasz.czarny.czarnecki@gmail.com>
2019-10-13 11:01:45 +0200

    Post method seems to be working

--- js/app.js
+++ js/app.js
@@ -2,28 +2,38 @@ document.addEventListener("DOMContentLoaded", function () {
     console.log("DOM content fully loaded and parsed")
 
     ajaxMethod()
+    addSubmitListener()
 
 })
 
 
-function ajaxMethod(methodParam) {
+function ajaxMethod(methodParam,book) {
 
+    let data
     // console.log(methodParam)
     let ajaxParam = ""
     if (methodParam === undefined) {
         ajaxParam = "GET"
-
-
+        data = null
+    }
+    else if(methodParam === "POST"){
+        ajaxParam = "POST"
+        data = JSON.stringify(book)
+    }
         $.ajax({
             url: "http://localhost:8282/books",
+            data: data,
             type: ajaxParam,
             dataType: "json",
             contentType: "application/json"
         }).done(function (data) {
 
             if (ajaxParam = "GET") {
-                printBooksList(data)
+                printBooks(data)
             }
+            // else if(ajaxParam = "POST"){
+            //     printBooks(data)
+            // }
 
         }).fail(function (xhr, status, err) {
             console.log("błąd")
@@ -33,37 +43,52 @@ function ajaxMethod(methodParam) {
 
         })
 
-
-    }
 }
 
-function printBooksList(data) {
+function addSubmitListener() {
+
+    let form = document.querySelector("form");
+    form.addEventListener("submit", function (event) {
+
+        let book = getFormParams()
+        console.log(book)
+
+        //sprawdzamy czy id z formularza puste, jeśli tak to POST, jeśli nie to PUT
+        let formId = document.getElementById("id")
+        if(formId.dataset.id === undefined ){
+           // wysyłamy POSTEm
+            ajaxMethod("POST",book)
+        }
+
+    })
+
+}
+
+function printBooks(data) {
 
     let ulEl = document.createElement("ul");
-
-
 
     data.forEach(book => {
 
         let {newLiEl, newDivEl, newDeleteButton, newModifyButton} = createElements();
 
-    newLiEl.dataset.id = book.id
-    newLiEl.dataset.isbn = book.isbn
-    newLiEl.dataset.title = book.title
-    newLiEl.dataset.author = book.author
-    newLiEl.dataset.publisher = book.publisher
-    newLiEl.dataset.type = book.type
+        newLiEl.dataset.id = book.id
+        newLiEl.dataset.isbn = book.isbn
+        newLiEl.dataset.title = book.title
+        newLiEl.dataset.author = book.author
+        newLiEl.dataset.publisher = book.publisher
+        newLiEl.dataset.type = book.type
 
-    newLiEl.innerText = "Nazwa: " + book.title + " || Autor: " + book.author
+        newLiEl.innerText = "Nazwa: " + book.title + " || Autor: " + book.author
 
-    newDivEl.innerText = "Id " + book.id + " || Isbn " + book.isbn + " || Publisher " + book.publisher + " || Type " + book.type
+        newDivEl.innerText = "Id " + book.id + " || Isbn " + book.isbn + " || Publisher " + book.publisher + " || Type " + book.type
 
-    newLiEl.appendChild(newDivEl)
-    newLiEl.appendChild(newDeleteButton)
-    newLiEl.appendChild(newModifyButton)
-    ulEl.appendChild(newLiEl)
-});
-    document.querySelector("body").insertBefore(ulEl,document.querySelector("form"))
+        newLiEl.appendChild(newDivEl)
+        newLiEl.appendChild(newDeleteButton)
+        newLiEl.appendChild(newModifyButton)
+        ulEl.appendChild(newLiEl)
+    });
+    document.querySelector("body").insertBefore(ulEl, document.querySelector("form"))
 
 }
 
@@ -82,3 +107,15 @@ function createElements() {
     newModifyButton.classList.add("modify")
     return {newLiEl, newDivEl, newDeleteButton, newModifyButton};
 }
+
+function getFormParams() {
+
+    let isbn = document.getElementById("isbn").value
+    let title = document.getElementById("title").value
+    let author = document.getElementById("author").value
+    let publisher = document.getElementById("publisher").value
+    let type = document.getElementById("type").value
+
+    return {isbn, title, author, publisher, type}
+}
+
